325e58d271f01b054ae6bd67c457ef308dd5590c
leiming <2008weileiming@gmail.com>
2020-09-27 16:00:50 +0800

    fix: 命名错误

--- foodie-mapper/src/main/java/com/lmfirst/mapper/ItemsMapperCustom.java
+++ foodie-mapper/src/main/java/com/lmfirst/mapper/ItemsMapperCustom.java
@@ -18,6 +18,6 @@ public interface ItemsMapperCustom {
 
     public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List<String> specIdsList);
 
-    public int decreaseItemSpecStock(@Param("spedId") String specId, @Param("pendingCounts") int pendingCounts);
+    public int decreaseItemSpecStock(@Param("specId") String specId, @Param("pendingCounts") int pendingCounts);
 
 }
